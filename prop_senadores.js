30f8b294d94f85f63901159b7525e8fb6cc0880a
denis <dineresc@gmail.com>
2020-05-21 12:21:52 -0300

    codigo nao utilizavel ainda para buscar proposicao de senador

---
+++ prop_senadores.js
@@ -0,0 +1,7 @@
+const data = document.getElementsByClassName('div-zebra')[0].getElementsByTagName('dl')[0].getElementsByTagName('dd')[3].innerText;
+
+const autor = document.getElementsByClassName('div-zebra')[0].getElementsByTagName('dl')[0].getElementsByTagName('dd')[2].innerText;
+
+const pl = document.getElementsByClassName('div-zebra')[0].getElementsByTagName('dl')[0].getElementsByTagName('dd')[0].getElementsByTagName('a')[0].innerText;
+
+const desc = document.getElementsByClassName('div-zebra')[0].getElementsByTagName('dl')[0].getElementsByTagName('dd')[1].innerText;
