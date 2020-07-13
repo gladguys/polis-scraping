4dd745094c7bd570220138c9bede56af018057a4
denis <dineresc@gmail.com>
2020-07-13 01:47:16 -0300

    add percentual no json de retorno

--- despesa_por_cotas.js
+++ despesa_por_cotas.js
@@ -1,20 +1,15 @@
 const puppeteer = require('puppeteer')
 
 module.exports = async (politicoId) => {
-  console.log("politico: " + politicoId);
   const browser = await puppeteer.launch({
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-    ],
+    args: [ '--no-sandbox','--disable-setuid-sandbox',]
   });
   const page = await browser.newPage();
   await page.goto('https://www.camara.leg.br/transparencia/gastos-parlamentares?legislatura=56&ano=2020&mes=&por=deputado&deputado='+politicoId+'&uf=&partido=');
-  await page.waitForSelector('#js-tipo-despesa-chart',{timeout:3000}).catch(() => console.log('Não foi possivel achar informação necessaria.'));
+  await page.waitForSelector('#js-tipo-despesa',{timeout:3000}).catch(() => console.log('Não foi possivel achar informação necessaria.'));
   
   const result = await page.evaluate(() => {
-    const importantContent = document.getElementById('js-tipo-despesa-chart');
-    const table = importantContent.getElementsByTagName('table')[0];
+    const table = document.getElementById('js-tipo-despesa');
     const trs = table.getElementsByTagName('tr');
     const infos = [];
     
@@ -22,7 +17,8 @@ module.exports = async (politicoId) => {
         let tds = trs[index].getElementsByTagName('td');
         let info = {
             "tipoCota": tds[0].textContent,
-            "valor": tds[1].textContent
+            "valor": tds[1].textContent,
+            "percentual": tds[2].textContent
         };
         infos.push(info);
     }
